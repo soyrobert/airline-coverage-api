e666aff7ce36f6ec91885f4a1f15be5044916aea
Robert Castro <hola@soyrobert.co>
2025-05-17 16:50:37 -0500

    feat: implement AirportService with CRUD operations and error handling

--- src/airport/airport.module.ts
+++ src/airport/airport.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirportEntity } from './airport.entity/airport.entity';
+import { AirportService } from './airport.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AirportEntity])],
+  providers: [AirportService],
 })
 export class AirportModule {}
