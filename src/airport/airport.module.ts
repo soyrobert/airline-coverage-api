3d73248cdbe09726d46973dcf946a73f04f1589c
Robert Castro <hola@soyrobert.co>
2025-05-17 20:15:53 -0500

    feat: add AirportController, AirportDto, and update AirportModule with CRUD operations

--- src/airport/airport.module.ts
+++ src/airport/airport.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirportEntity } from './airport.entity/airport.entity';
 import { AirportService } from './airport.service';
+import { AirportController } from './airport.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AirportEntity])],
   providers: [AirportService],
   exports: [AirportService],
+  controllers: [AirportController],
 })
 export class AirportModule {}
