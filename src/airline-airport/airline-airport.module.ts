3936fdbde7f19a735269134c3611479a95bc0dcc
Robert Castro <hola@soyrobert.co>
2025-05-17 20:27:01 -0500

    feat: implement AirlineAirportController with CRUD operations in AirlineAirportModule

--- src/airline-airport/airline-airport.module.ts
+++ src/airline-airport/airline-airport.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirlineEntity } from '../airline/airline.entity/airline.entity';
 import { AirportEntity } from '../airport/airport.entity/airport.entity';
 import { AirlineAirportService } from './airline-airport.service';
+import { AirlineAirportController } from './airline-airport.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AirlineEntity, AirportEntity])],
   providers: [AirlineAirportService],
   exports: [AirlineAirportService],
+  controllers: [AirlineAirportController],
 })
 export class AirlineAirportModule {}
